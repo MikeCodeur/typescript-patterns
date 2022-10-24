b9ab7d1b35e76907b12c75c06b96e792158830dd
Mike Codeur <cabanas.mickael@gmail.com>
2022-10-24 10:17:10 +0800

    exo1

--- src/exercise/01.tsx
+++ src/exercise/01.tsx
@@ -1,32 +1,49 @@
-// Styling
-/*
+// Les opérateurs
+// http://localhost:3000/alone/exercise/01.ts
 
-*/
+// ❌ NE PAS MODIFIER
+// Utilitaire ne faisant pas partie de l'exercice
+import displayText, {init} from './helper/exerciseHelper'
+init()
 
-import React from 'react'
+// ✔️ Début de l'exercice
 
-type Personne = {
-  age: number
-  name: string
+// 🐶 Modifie 'DataBaseConnexion' pour qu'il devienne unb Singleton
+class DataBaseConnexion {
+  // 🐶 créé un champs 'instance' privé et static de type 'DataBaseConnexion'
+  // 🐶 déclare ici en privé 'hostname' 'port' 'protocol' et supprime les du constructeur
+  // 🐶 rend le constructeur privé
+  public constructor(
+    // ⛏️ supprime les 3 paramètre du constructeur
+    public hostname: string,
+    public port: string,
+    public protocol: string,
+  ) {
+    console.log(
+      `Connexion à la BDD ${this.protocol} ${this.hostname} ${this.port}`,
+    )
+    displayText(
+      `Connexion à la BDD ${this.protocol} ${this.hostname} ${this.port}`,
+    )
+  }
+  // 🐶 créé une méthode 'getInstance' qui instancie une seule fois 'DataBaseConnexion'
+  // et retourne 'instance'
+  public disconnect() {
+    console.log(
+      `disconnect from ${this.protocol} ${this.hostname} ${this.port}`,
+    )
+  }
+  public execute() {
+    console.log(`execute on ${this.protocol} ${this.hostname} ${this.port}`)
+  }
 }
-const p: Personne = {age: 21, name: 'mike'}
-
-const smallBox = <div>small box {p.name}</div>
-const mediumBox = <div>medium box</div>
-const largeBox = <div>large box</div>
-
-function Box() {
-  return (
-    <div>
-      {smallBox}
-      {mediumBox}
-      {largeBox}
-    </div>
-  )
-}
-
-export default Box
-
+// 🐶 modifie les 3 instanciation
+const con = new DataBaseConnexion('localhost', '3301', 'mysql')
+con.execute()
+const con2 = new DataBaseConnexion('localhost', '3301', 'mysql')
+con2.execute()
+const con3 = new DataBaseConnexion('localhost', '3301', 'mysql')
+con2.execute()
 /*
 eslint
   @typescript-eslint/no-unused-vars: "off"
